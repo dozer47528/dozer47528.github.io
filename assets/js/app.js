736d4281e729e346fa1e1e0d8e3c273035ca8e37
Dozer <mail@dozer.cc>
2016-01-27 17:08:48 +0800

    add fancybox

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,30 @@
-$( document ).ready(function() {
+$(function() {
+  hljs.initHighlightingOnLoad();
 
-	/* Sidebar height set */
-	$('.sidebar').css('min-height',$(document).height());
+  $('#toc').toc({
+    'selectors': 'h3,h4', //elements to use as headings
+    'container': '.article_body', //element to find all selectors in
+    'smoothScrolling': true, //enable or disable smooth scrolling on click
+    'prefix': 'toc', //prefix for anchor tags and class names
+    'onHighlight': function(el) {}, //called when a new section is highlighted
+    'highlightOnScroll': true, //add class to heading that is currently in focus
+    'highlightOffset': 100, //offset to trigger the next headline
+    'anchorName': function(i, heading, prefix) { //custom function for anchor name
+      return prefix + i;
+    },
+    'headerText': function(i, heading, $heading) { //custom function building the header-item text
+      return $heading.text();
+    },
+    'itemClass': function(i, heading, $heading, prefix) { // custom function for item class
+      return $heading[0].tagName.toLowerCase();
+    }
+  });
 
-	/* Secondary contact links */
-	var scontacts = $('#contact-list-secondary');
-	var contact_list = $('#contact-list');
-	
-	scontacts.hide();
-	
-	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
-	
-	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
+  $.each($('.article_body>p>img'), function(index, value) {
+    var item = $(value);
+    var url = item.attr('src');
+    item.wrap('<a class="fancybox" href="' + url + '"></a>')
+  });
 
+  $('.fancybox').fancybox();
 });
