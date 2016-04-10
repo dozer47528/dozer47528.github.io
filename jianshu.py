599e6979743708a829f14ed080baa0bc863af0fd
Dozer <mail@dozer.cc>
2016-04-10 17:37:53 +0800

    Add jianshu convert script.

---
+++ jianshu.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+
+file_name = sys.argv[1]
+
+file = open(file_name, 'r')
+
+content = file.read()
+
+link = "http://www.dozer.cc" + re.search('permalink:\s*(.*)', content).group(1)
+
+content = re.sub('---.*---', '', content, flags=re.S)
+
+content = re.sub('<!--more-->', '', content)
+
+content = re.sub('\]\(/uploads/', '](http://www.dozer.cc/uploads/', content)
+
+content += '\n&nbsp;\n\n源地址:[{link}]({link})'.format(link=link)
+
+print content
