6cf9dd84e8659281bfe7fa1133d3ca8766d2f402
Dozer <mail@dozer.cc>
2020-02-29 19:33:18 +0800

    [Feature] Change weixin script.

--- weixin.py
+++ weixin.py
@@ -1,3 +1,4 @@
+#!python
 # -*- coding: utf-8 -*-
 
 import sys
@@ -9,13 +10,24 @@ file = open(file_name, 'r')
 
 content = file.read()
 
-link = "http://www.dozer.cc" + re.search('permalink:\s*(.*)', content).group(1)
-
 content = re.sub('---.*---', '', content, flags=re.S)
 
 content = re.sub('<!--more-->', '', content)
 
-content = re.sub('\]\(/uploads/', '](http://www.dozer.cc/uploads/', content)
+# Replace image with base64
+for img_exp in re.findall("!\[[^\]]*\]\([^\)]*\)",content):
+    img_path = img_exp[:-1].split("(")[1][1:]
+    with open(img_path, 'r') as img_file:
+        img_base64 = img_file.read().encode('base64').replace("\n", "")
+        img_html = '<img src="data:image/jpeg;base64, %s" />' % img_base64
+        content = content.replace(img_exp, img_html)
+
+# Replace external link
+for url in re.findall("\[[^\]]*\]\([^\)]*\)",content):
+    url_title, url_link = url[1:-1].split("](")
+    if str(url_link).startswith("/"):
+        url_link = "https://www.dozer.cc" + url_link
+    content = content.replace(url, " %s %s " % (url_title, url_link))
 
 content += '\n&nbsp;\n\n👇更好的排版请点击原文连接👇'
 
